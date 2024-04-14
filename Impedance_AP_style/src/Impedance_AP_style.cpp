6f4abe170cdb54ed62273d3cf2c5cad4075b2b22
Laura Green <laura.eliza.green@gmail.com>
2024-04-14 15:42:16 -0600

    using sin wave instead of square wave

--- Impedance_AP_style/src/Impedance_AP_style.cpp
+++ Impedance_AP_style/src/Impedance_AP_style.cpp
@@ -8,14 +8,21 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include<math.h>
 
-const int PULSEPIN=A1;
+const int PULSEPIN=A5;//PWM pin
 const int PULSEREADPIN=A2;
-const int PLANTREADPIN=A5;
-const int AVVOLT=127;//average voltage applied to pulse pin 127 produces equal on off (square wave?)
-float hz;//start hz at 100 (like multipip range is 5-50,000 Hz)
+const int PLANTREADPIN=A1;
+const int AVSIG=127;//average signal applied to pulse pin 127 produces equal on off (square wave?)
+float hz;//start hz at 100 (like multipip range is 5-50,000 Hz) Adrian started with 500Hz
 float pulse;
 float plant;
+float ratio;
+float sinwave;
+float A;
+float t;
+float v;//frequency
+float B;//offset
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
@@ -31,14 +38,23 @@ void setup() {
   pinMode(PULSEPIN,OUTPUT);
   pinMode(PULSEREADPIN,INPUT);
   pinMode(PLANTREADPIN, INPUT);
+
+  hz=500;
+  A=255/2.0;//amplitude
+  t=millis()/1000.0; //time in seconds
+  v=500;//frequency
+  B=255/2.0;//offset
 }
 
 
 void loop() {
-  analogWrite(PULSEPIN,AVVOLT,hz);
+  sinwave=A* sin(2 * M_PI * v * t)+B;
+  //analogWrite(PULSEPIN,AVSIG,hz);
+  analogWrite(PULSEPIN,sinwave);//why is amplitude btw 0-255, but output 0-4096?
   pulse=analogRead(PULSEREADPIN);
   plant=analogRead(PLANTREADPIN);
-  Serial.printf("PULSE=%f\nPLANT=%f\n",pulse,plant);
+  ratio=plant/pulse;
+  Serial.printf("PULSE=%.2f\nPLANT=%.2f\nRATIO=%.2f\n",pulse,plant,ratio);//usually btw 0-3 occasionally higher (22..)
   delay(1000);
   
 }
