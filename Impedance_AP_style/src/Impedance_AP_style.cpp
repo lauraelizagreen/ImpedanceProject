5e5a8669ebbac66e0ec4c5862c3a8b1b7265e764
Laura Green <laura.eliza.green@gmail.com>
2024-04-21 15:23:49 -0600

    re wrote publishing code that was lost

--- Impedance_AP_style/src/Impedance_AP_style.cpp
+++ Impedance_AP_style/src/Impedance_AP_style.cpp
@@ -9,6 +9,19 @@
 // Include Particle Device OS APIs
 #include "Particle.h"//need to have credentials in ignore file
 #include<math.h>
+#include <Adafruit_MQTT.h>
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "credentials.h"
+
+TCPClient TheClient; 
+
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+
+Adafruit_MQTT_Publish pubFeedZDataFreq = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ZDataFreq");
+Adafruit_MQTT_Publish pubFeedZDataPulse = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ZDataPulse");
+Adafruit_MQTT_Publish pubFeedZDataPlant = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ZDataPlant");
+Adafruit_MQTT_Publish pubFeedZDataRatio = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ZDataRatio");
 
 const int PULSEPIN=A5;//PWM pin
 const int PULSEREADPIN=A2;
@@ -25,10 +38,14 @@ float A;
 float t;
 float v;//frequency
 float B;//offset
+unsigned int lastTimePub;
 /////////function to read AP style impedance (ratio)///////
 float ratReadArray[100][2];//array will contain freq and max ratio for 100 freqs (steps of 500hz)
 
 float ratAPRead(hz);//declare function
+
+void MQTT_connect();
+bool MQTT_ping();
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
@@ -55,6 +72,9 @@ void setup() {
 
 
 void loop() {
+   MQTT_connect();
+  MQTT_ping();
+
   //sinwave=A* sin(2 * M_PI * v * t)+B; //for sin wave, but won't be able to get high enough frequencies
 
  
@@ -66,6 +86,14 @@ void loop() {
     i++;
     hz=hz+500;
      }
+     if((millis()-lastTimePub > 6000)) {//publishing (how often?)
+    if(mqtt.Update()) {
+      pubFeedZDataFreq.publish(hz);//publish Hz
+      pubFeedZDataRatio.publish(maxRatio);//publish max Freq
+      Serial.printf("Publishing %.2f at %.2f\n",maxRatio,hz); 
+      } 
+    lastTimePub = millis();
+  }
   
   //analogWrite(PULSEPIN,AVSIG,hz);//replace with function
   //analogWrite(PULSEPIN,sinwave);//why is amplitude btw 0-255, but output 0-4096?
@@ -103,6 +131,41 @@ startRead=millis();
   }
   return ratioMax;
 }
+void MQTT_connect() {//actually connects to server, if not connected stuck in loop
+  int8_t ret;//photon 2 thinks of integers as 32bits
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
+
+bool MQTT_ping() {//broker will disconnect if doesn't hear anything, just reminding broker still here so don't disconnect
+  static unsigned int last;
+  bool pingStatus;
+
+  if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+  }
+  return pingStatus;
+}
+
   
 
 
